3f70deaf5210e118359b117d4d00173147994d46
chengshuo.wang <chengshuo.wang@amlogic.com>
2022-05-18 17:37:02 +0800

    wifi: change ap name from p2p0 to ap0 [2/4]
    PD#SWPL-65026
    
    Problem:
    wifi ap name change
    
    Solution:
    change w1 and qca6174 ap name from p2p0 to ap0
    
    Verify:
    S905Y4
    
    Signed-off-by: chengshuo.wang <chengshuo.wang@amlogic.com>
    Change-Id: I2c6e19fa89c96a1976e4384a062510f49141a9b0

--- wifi_bt/wifi/wifi_info/wifi_info.cc
+++ wifi_bt/wifi/wifi_info/wifi_info.cc
@@ -54,7 +54,7 @@ char *no_base = NULL;
 static const dongle_info dongle_registerd[]={\
     {"0000","0000","sprdwl_ng","/vendor/lib/modules/sprdwl_ng.ko","",uwe5621_base,"uwe5621ds",0x0,""},\
     {"a962","0000","dhd","/vendor/lib/modules/dhd.ko","firmware_path=/vendor/etc/wifi/40181/fw_bcm40181a2.bin nvram_path=/vendor/etc/wifi/40181/nvram.txt",no_base,"bcm6210",0x0,"/vendor/etc/wifi/40181/fw_bcm40181a2"},\
-    {"0000","0000","wlan_mt76x8_usb","/vendor/lib/modules/wlan_mt76x8_usb.ko","sta=wlan ap=wlan p2p=p2p",no_base,"mtk7668u",0x7668,""},\
+    {"0000","0000","wlan_mt76x8_usb","/vendor/lib/modules/wlan_mt76x8_usb.ko","sta=wlan ap=ap p2p=p2p",no_base,"mtk7668u",0x7668,""},\
     {"4335","0000","dhd","/vendor/lib/modules/dhd.ko","firmware_path=/vendor/etc/wifi/6335/fw_bcm4339a0_ag.bin nvram_path=/vendor/etc/wifi/6335/nvram.txt",no_base,"bcm6335",0x0,"/vendor/etc/wifi/6335/fw_bcm4339a0_ag"},\
     {"0000","43c5","dhdpci","/vendor/lib/modules/dhdpci.ko","firmware_path=/vendor/etc/wifi/4336/fw_bcm4336_ag.bin nvram_path=/vendor/etc/wifi/4336/nvram.txt",no_base,"bcm4336",0x0,"/vendor/etc/wifi/4336/fw_bcm4336_ag"},\
     {"a94d","0000","dhd","/vendor/lib/modules/dhd.ko","firmware_path=/vendor/etc/wifi/6234/fw_bcm43341b0_ag.bin nvram_path=/vendor/etc/wifi/6234/nvram.txt",no_base,"bcm6234",0x0,"/vendor/etc/wifi/6234/fw_bcm43341b0_ag"},\
@@ -68,62 +68,62 @@ static const dongle_info dongle_registerd[]={\
     {"4359","0000","dhd","/vendor/lib/modules/dhd.ko","firmware_path=/vendor/etc/wifi/4359/fw_bcm4359c0_ag.bin nvram_path=/vendor/etc/wifi/4359/nvram.txt",no_base,"bcm4359",0x0,"/vendor/etc/wifi/4359/fw_bcm4359c0_ag"},\
     {"0000","4415","dhdpci","/vendor/lib/modules/dhdpci.ko","firmware_path=/vendor/etc/wifi/4359/fw_bcm4359c0_ag.bin nvram_path=/vendor/etc/wifi/4359/nvram.txt",no_base,"bcm4359",0x0,"/vendor/etc/wifi/4359/fw_bcm4359c0_ag"},\
     {"aa31","0000","dhd","/vendor/lib/modules/dhd.ko","firmware_path=/vendor/etc/wifi/4358/fw_bcm4358_ag.bin nvram_path=/vendor/etc/wifi/4358/nvram_4358.txt",no_base,"bcm4358",0x0,"/vendor/etc/wifi/4358/fw_bcm4358_ag"},\
-    {"8888","0000","vlsicomm","/vendor/lib/modules/vlsicomm.ko","vmac0=wlan0 vmac1=p2p0",aml_w1_base,"aml_w1",0x0,""},\
-    {"8888","0000","w1u","/vendor/lib/modules/w1u.ko","vmac0=wlan0 vmac1=p2p0",aml_w1u_s_base,"aml_w1u_s",0x0,""},\
-    {"0000","0000","w1u","/vendor/lib/modules/w1u.ko","vmac0=wlan0 vmac1=p2p0",aml_w1u_base,"aml_w1u",0x4c55,""},\
-    {"8179","0000","8189es","/vendor/lib/modules/8189es.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8189es",0x0,""},\
-    {"b723","0000","8723bs","/vendor/lib/modules/8723bs.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8723bs",0x0,""},\
-    {"c723","0000","8723cs","/vendor/lib/modules/8723cs.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8723cs",0x0,""},\
-    {"f179","0000","8189fs","/vendor/lib/modules/8189fs.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8189ftv",0x0,""},\
-    {"818b","0000","8192es","/vendor/lib/modules/8192es.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8192es",0x0,""},\
-    {"0000","0000","8188eu","/vendor/lib/modules/8188eu.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8188eu",0x8179,""},\
-    {"0000","0000","8188gtvu","/vendor/lib/modules/8188gtvu.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8188gtv",0x018c,""},\
+    {"8888","0000","vlsicomm","/vendor/lib/modules/vlsicomm.ko","vmac0=wlan0 vmac1=ap0",aml_w1_base,"aml_w1",0x0,""},\
+    {"8888","0000","w1u","/vendor/lib/modules/w1u.ko","vmac0=wlan0 vmac1=ap0",aml_w1u_s_base,"aml_w1u_s",0x0,""},\
+    {"0000","0000","w1u","/vendor/lib/modules/w1u.ko","vmac0=wlan0 vmac1=ap0",aml_w1u_base,"aml_w1u",0x4c55,""},\
+    {"8179","0000","8189es","/vendor/lib/modules/8189es.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8189es",0x0,""},\
+    {"b723","0000","8723bs","/vendor/lib/modules/8723bs.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8723bs",0x0,""},\
+    {"c723","0000","8723cs","/vendor/lib/modules/8723cs.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8723cs",0x0,""},\
+    {"f179","0000","8189fs","/vendor/lib/modules/8189fs.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8189ftv",0x0,""},\
+    {"818b","0000","8192es","/vendor/lib/modules/8192es.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8192es",0x0,""},\
+    {"0000","0000","8188eu","/vendor/lib/modules/8188eu.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8188eu",0x8179,""},\
+    {"0000","0000","8188gtvu","/vendor/lib/modules/8188gtvu.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8188gtv",0x018c,""},\
     {"0000","0000","atbm602x_usb","/vendor/lib/modules/atbm602x_usb.ko","",no_base,"atbm602x",0x8888,""},\
-    {"0000","0000","8188eu","/vendor/lib/modules/8188eu.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8188eu",0x0179,""},\
+    {"0000","0000","8188eu","/vendor/lib/modules/8188eu.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8188eu",0x0179,""},\
     {"0000","0000","mt7601usta","/vendor/lib/modules/mt7601usta.ko","",mtk760_base,"mtk7601",0x7601,""},\
     {"0000","0000","mt7603usta","/vendor/lib/modules/mt7603usta.ko","",mtk760_base,"mtk7603",0x7603,""},\
     {"0000","0000","mt7662u_sta","/vendor/lib/modules/mt7662u_sta.ko","",no_base,"mtk7632",0x76a0,""},\
     {"0000","0000","mt7662u_sta","/vendor/lib/modules/mt7662u_sta.ko","",no_base,"mtk7632",0x76a1,""},\
-    {"c821","0000","8821cs","/vendor/lib/modules/8821cs.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8821cs",0x0,""},\
-    {"b821","0000","8821cs","/vendor/lib/modules/8821cs.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8821cs",0x0,""},\
-    {"0000","0000","8723du","/vendor/lib/modules/8723du.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8723du",0xd723,""},\
-    {"d723","0000","8723ds","/vendor/lib/modules/8723ds.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8723ds",0x0,""},\
-    {"0000","0000","8821au","/vendor/lib/modules/8821au.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8821au",0x0823,""},\
-    {"0000","0000","8821au","/vendor/lib/modules/8821au.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8821au",0x0821,""},\
-    {"0000","0000","8821au","/vendor/lib/modules/8821au.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8821au",0x0811,""},\
-    {"0000","0000","8812au","/vendor/lib/modules/8812au.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8812au",0x881a,""},\
-    {"c822","0000","8822cs","/vendor/lib/modules/8822cs.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8822cs",0x0,""}, \
-    {"0000","0000","8188fu","/vendor/lib/modules/8188fu.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8188ftv",0xf179,""},\
-    {"0000","0000","8192eu","/vendor/lib/modules/8192eu.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8192eu",0x818b,""},\
-    {"0000","0000","8192fu","/vendor/lib/modules/8192fu.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8192fu",0xf192,""},\
-    {"b822","0000","8822bs","/vendor/lib/modules/8822bs.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8822bs",0x0,""},\
-    {"0000","0000","8733bu","/vendor/lib/modules/8733bu.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8733bu",0xb733,""},\
-    {"0000","0000","8852au","/vendor/lib/modules/8852au.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8852au",0x885c,""},\
-    {"0000","0000","8852au","/vendor/lib/modules/8852au.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8852au",0x885a,""},\
+    {"c821","0000","8821cs","/vendor/lib/modules/8821cs.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8821cs",0x0,""},\
+    {"b821","0000","8821cs","/vendor/lib/modules/8821cs.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8821cs",0x0,""},\
+    {"0000","0000","8723du","/vendor/lib/modules/8723du.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8723du",0xd723,""},\
+    {"d723","0000","8723ds","/vendor/lib/modules/8723ds.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8723ds",0x0,""},\
+    {"0000","0000","8821au","/vendor/lib/modules/8821au.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8821au",0x0823,""},\
+    {"0000","0000","8821au","/vendor/lib/modules/8821au.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8821au",0x0821,""},\
+    {"0000","0000","8821au","/vendor/lib/modules/8821au.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8821au",0x0811,""},\
+    {"0000","0000","8812au","/vendor/lib/modules/8812au.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8812au",0x881a,""},\
+    {"c822","0000","8822cs","/vendor/lib/modules/8822cs.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8822cs",0x0,""}, \
+    {"0000","0000","8188fu","/vendor/lib/modules/8188fu.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8188ftv",0xf179,""},\
+    {"0000","0000","8192eu","/vendor/lib/modules/8192eu.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8192eu",0x818b,""},\
+    {"0000","0000","8192fu","/vendor/lib/modules/8192fu.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8192fu",0xf192,""},\
+    {"b822","0000","8822bs","/vendor/lib/modules/8822bs.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8822bs",0x0,""},\
+    {"0000","0000","8733bu","/vendor/lib/modules/8733bu.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8733bu",0xb733,""},\
+    {"0000","0000","8852au","/vendor/lib/modules/8852au.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8852au",0x885c,""},\
+    {"0000","0000","8852au","/vendor/lib/modules/8852au.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8852au",0x885a,""},\
     {"0701","0000","wlan","/vendor/lib/modules/wlan_9377.ko","",no_base,"qca9377",0x0,""},\
-    {"050a","0000","wlan","/vendor/lib/modules/wlan_6174.ko","country_code=CN",no_base,"qca6174",0x0,""},\
+    {"050a","0000","wlan","/vendor/lib/modules/wlan_6174.ko","ap_name=ap0 country_code=CN",no_base,"qca6174",0x0,""},\
     {"0801","0000","wlan","/vendor/lib/modules/wlan_9379.ko","",no_base,"qca9379",0x0,""},\
     {"0000","0000","wlan","/vendor/lib/modules/wlan_9379.ko","",no_base,"qca9379",0x9378,""},\
     {"0000","0000","wlan","/vendor/lib/modules/wlan_9379.ko","",no_base,"qca9379",0x7a85,""},\
-    {"7608","0000","wlan_mt76x8_sdio","/vendor/lib/modules/wlan_mt76x8_sdio.ko","sta=wlan ap=wlan p2p=p2p",no_base,"mtk7668s",0x0,""},\
+    {"7608","0000","wlan_mt76x8_sdio","/vendor/lib/modules/wlan_mt76x8_sdio.ko","sta=wlan ap=ap p2p=p2p",no_base,"mtk7668s",0x0,""},\
     {"7603","0000","wlan_mt7663_sdio","/vendor/lib/modules/wlan_mt7663_sdio.ko","",no_base,"mtk7661s",0x0,""},\
     {"037a","0000","wlan_mt7663_sdio","/vendor/lib/modules/wlan_mt7663_sdio.ko","",no_base,"mtk7661s",0x0,""},\
     {"0000","0000","wlan_mt7663_usb","/vendor/lib/modules/wlan_mt7663_usb.ko","",mtk7663u_base,"mtk7663u",0x7663,""},\
     {"0000","0000","bcmdhd","/vendor/lib/modules/bcmdhd.ko","firmware_path=/vendor/etc/wifi/43569/fw_bcm4358u_ag.bin nvram_path=/vendor/etc/wifi/43569/nvram_ap62x8.txt dhd_pwr_ctrl=0",no_base,"bcm43569",0xbd27,"/vendor/etc/wifi/43569/fw_bcm4358u_ag"}, \
     {"0000","0000","bcmdhd","/vendor/lib/modules/bcmdhd.ko","firmware_path=/vendor/etc/wifi/43569/fw_bcm4358u_ag.bin nvram_path=/vendor/etc/wifi/43569/nvram_ap62x8.txt dhd_pwr_ctrl=0",no_base,"bcm43569",0x0bdc,"/vendor/etc/wifi/43569/fw_bcm4358u_ag"}, \
-    {"0000","0000","8723bu","/vendor/lib/modules/8723bu.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8723bu",0xb720,""}, \
-    {"0000","0000","8822bu","/vendor/lib/modules/8822bu.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8822bu",0xb82c,""}, \
-    {"0000","0000","88x2cu","/vendor/lib/modules/88x2cu.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl88x2cu",0xc82c,""}, \
-    {"0000","0000","8821cu","/vendor/lib/modules/8821cu.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8821cu",0xc820,""}, \
-    {"0000","0000","8821cu","/vendor/lib/modules/8821cu.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8821cu",0xc811,""}, \
+    {"0000","0000","8723bu","/vendor/lib/modules/8723bu.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8723bu",0xb720,""}, \
+    {"0000","0000","8822bu","/vendor/lib/modules/8822bu.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8822bu",0xb82c,""}, \
+    {"0000","0000","88x2cu","/vendor/lib/modules/88x2cu.ko","ifname=wlan0 if2name=ap0",no_base,"rtl88x2cu",0xc82c,""}, \
+    {"0000","0000","8821cu","/vendor/lib/modules/8821cu.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8821cu",0xc820,""}, \
+    {"0000","0000","8821cu","/vendor/lib/modules/8821cu.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8821cu",0xc811,""}, \
     {"3030","0000","ssv_hwif_ctrl","/vendor/lib/modules/ssv_hwif_ctrl.ko",no_base,ssv6051_base,"ssv6051",0x0,""}, \
     {"0000","449d","dhdpci","/vendor/lib/modules/dhdpci.ko","firmware_path=/vendor/etc/wifi/43752a2/fw_bcm43752a2_pcie_ag.bin nvram_path=/vendor/etc/wifi/43752a2/nvram_ap6275p.txt",no_base,"bcm43752a2p",0x0,"/vendor/etc/wifi/43752a2/fw_bcm43752a2_pcie_ag"},\
     {"0000","4475","dhdpci","/vendor/lib/modules/dhdpci.ko","firmware_path=/vendor/etc/wifi/43752a2/fw_bcm4375b4_pcie_ag.bin nvram_path=/vendor/etc/wifi/43752a2/nvram_ap6275hh3.txt",no_base,"bcm43752a2p",0x0,"/vendor/etc/wifi/43752a2/fw_bcm4375b4_pcie_ag"},\
     {"aae8","0000","dhd","/vendor/lib/modules/dhd.ko","firmware_path=/vendor/etc/wifi/43752a2/fw_bcm43752a2_ag.bin nvram_path=/vendor/etc/wifi/43752a2/nvram_ap6275s.txt",no_base,"bcm43752a2s",0x0,"/vendor/etc/wifi/43752a2/fw_bcm43752a2_ag"},\
     {"0000","1101","wlan","/vendor/lib/modules/wlan_6391.ko","",qca6391_base,"qca6391",0x0,""},\
     {"0000","1103","wlan","/vendor/lib/modules/wlan_206x.ko","",qca206x_base,"qca206x",0x0,""},\
-    {"0000","8852","8852ae","/vendor/lib/modules/8852ae.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8852ae",0x0,""},\
-    {"0000","b852","8852be","/vendor/lib/modules/8852be.ko","ifname=wlan0 if2name=p2p0",no_base,"rtl8852be",0x0,""},\
+    {"0000","8852","8852ae","/vendor/lib/modules/8852ae.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8852ae",0x0,""},\
+    {"0000","b852","8852be","/vendor/lib/modules/8852be.ko","ifname=wlan0 if2name=ap0",no_base,"rtl8852be",0x0,""},\
     {"9149","0000","moal_sd8987","/vendor/lib/modules/moal_sd8987.ko","mod_para=nxp/wifi_mod_para_8987.conf",nxp8987_base,"nxp8987",0x0,""},\
     {"9141","0000","moal_sd8997","/vendor/lib/modules/moal_sd8997.ko","mod_para=nxp/wifi_mod_para_8997.conf",nxp8997_base,"nxp8997",0x0,""},\
     {"0000","2b56","moal_iw620","/vendor/lib/modules/moal_iw620.ko","mod_para=nxp/wifi_mod_para_iw620.conf",nxpiw620_base,"nxpiw620",0x0,""},\
