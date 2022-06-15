2c59ff2969b7e1485ea1e3e56e763a6d0677c544
yidong zhang <yidong.zhang@amlogic.com>
2022-06-15 13:52:45 +0800

    [LSC] Add LOCAL_LICENSE_KINDS to vendor/amlogic/common
    Added SPDX-license-identifier-Apache-2.0 SPDX-license-identifier-BSD
        SPDX-license-identifier-LGPL legacy_by_exception_only
    to:
      wifi_bt/bluetooth/unisoc/tools/bdt_unisoc/Android.mk
    
    Bug: 68860345
    Bug: 151177513
    Bug: 151953481
    
    Test: m all
    Change-Id: I47de147b192f1a0764380b0d6e2548ef4d35884d

--- wifi_bt/bluetooth/unisoc/tools/bdt_unisoc/Android.mk
+++ wifi_bt/bluetooth/unisoc/tools/bdt_unisoc/Android.mk
@@ -11,5 +11,7 @@ LOCAL_MODULE:= bdt_unisoc
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_SHARED_LIBRARIES +=
 LOCAL_PROPRIETARY_MODULE := true
-
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 SPDX-license-identifier-BSD SPDX-license-identifier-LGPL legacy_by_exception_only
+LOCAL_LICENSE_CONDITIONS := by_exception_only notice restricted
+LOCAL_NOTICE_FILE := $(LOCAL_PATH)/../../../../../LICENSE
 include $(BUILD_EXECUTABLE)
