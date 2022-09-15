6ca6cff5d8af9c320e4b0c8d96950ad43c2dc126
Robin Lee <rgl@google.com>
2022-09-15 22:45:49 +0200

    Remove atv_flatten_mainline_modules
    Google internal targets should use updatable mainline modules
    development of Android U and for testing on Android T.
    
    Bug: 245727313
    Bug: 196107237
    Change-Id: I72d0427c69ce5a1c7bd16207a39f605037f818c8

--- gms/google/gms.mk
+++ gms/google/gms.mk
@@ -21,7 +21,7 @@ ifeq ($(PRODUCT_USE_PREBUILT_GTVS), yes)
     $(call inherit-product, vendor/google_gtvs_gtv/products/gtvs_gtv.mk)
   endif
 else
-  $(call inherit-product-if-exists, vendor/google_atv/products/atv_flatten_mainline_modules.mk)
+  $(call inherit-product-if-exists, vendor/google_atv/products/atv_mainline_modules.mk)
   ifeq ($(ATV_LAUNCHER),amati)
     $(call inherit-product-if-exists, vendor/google_atv/products/atv_google_amati_3p.mk)
   else
