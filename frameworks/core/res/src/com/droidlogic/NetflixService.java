1ca3fbc5804b885b7d43d04e6ef4081de944fc7e
minghui.yan <minghui.yan@amlogic.com>
2022-12-08 15:39:01 +0800

    NetflixService: Can not identify Atmos. [1/1]
    PD#SWPL-103612
    
    Problem:
    dolby digital was spelling error.
    
    Solution:
    Dolby_Digital+/ATMOS, spelling correctly.
    
    Verify:
    S905Y4
    
    Change-Id: I5d3270eb7af5e555a1f82ccdbbd7d04d9e8fd95d
    Signed-off-by: minghui.yan <minghui.yan@amlogic.com>

--- frameworks/core/res/src/com/droidlogic/NetflixService.java
+++ frameworks/core/res/src/com/droidlogic/NetflixService.java
@@ -381,8 +381,8 @@ public class NetflixService extends Service {
         int surround = mOutputModeManager.getDigitalAudioFormatOut();
         Log.i(TAG, "onReceived HDMI_PLUGGED: " + isHdmiPlugged + ", surround:" + DroidLogicUtils.audioFormatOutputToString(surround));
         String audioSinkCap = mSCM.readSysFs(SYS_AUDIO_CAP);
-        atmosSupported = audioSinkCap.contains("Dobly_Digital+/ATMOS");
-        doblySupported = audioSinkCap.contains("Dobly_Digital");
+        atmosSupported = audioSinkCap.contains("Dolby_Digital+/ATMOS");
+        doblySupported = audioSinkCap.contains("Dolby_Digital");
         if (isHdmiPlugged && (OutputModeManager.DIGITAL_AUDIO_FORMAT_AUTO == surround
                 || OutputModeManager.DIGITAL_AUDIO_FORMAT_PASSTHROUGH == surround)) {
             Log.i(TAG, "ATMOS: " + atmosSupported + ", audioSinkCap: " + audioSinkCap);
