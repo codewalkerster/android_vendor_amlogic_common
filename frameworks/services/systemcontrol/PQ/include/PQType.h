fb1a0408cd58afeb12bb8f3743a9e4daa0c34bc9
chenfei.dou <chenfei.dou@amlogic.com>
2023-03-06 14:33:19 +0800

    pqtype: Android T add input_colorimetry in tv_info_S struct [1/1]
    PD#SWPL-92090
    
    Problem:
    vdin add input_colorimetry in tv_info_s
    
    Solution:
    pqtype add input_colorimetry in tv_info_s
    
    Verify:
    t5w
    
    Change-Id: Ia33d647e0f6eeaedbbca5752ee983119631494ff
    Signed-off-by: chenfei.dou <chenfei.dou@amlogic.com>

--- frameworks/services/systemcontrol/PQ/include/PQType.h
+++ frameworks/services/systemcontrol/PQ/include/PQType.h
@@ -902,6 +902,11 @@ typedef struct tvin_info_s {
      *  "smpte170m", "smpte240m", "YCgCo", "bt2020nc", "bt2020c"
      */
     unsigned int hdr_info;
+    /*
+    * 0:xvYCC601  1:xvYCC709  2:sYCC601  3:Adobe ycc601
+    * 4:Adobe rgb 5:BT2020(ycc) 6:BT2020(rgb or yuv) 7:reserved
+    */
+    unsigned int input_colorimetry;
     tvin_aspect_ratio_e aspect_ratio;
     //0:no dv 1:vsif 2:emp
     unsigned char amdolby_vision;
