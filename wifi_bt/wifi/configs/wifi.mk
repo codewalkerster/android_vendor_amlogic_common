080e5f5b3123b6a90819a440a8f5fdf0146a59bd
jiabin.chen <jiabin.chen@amlogic.com>
2023-04-04 11:14:43 +0800

    wifi: add w2 vendor id [1/1]
    PD#SWPL-117456
    
    Problem:
    can not insmod w2 driver
    
    Solution:
    add w2 vendor id
    
    Verify:
    T210
    
    Change-Id: Ia9eaa420fb34cce8902de3f1bb6e8965ac8c6c89
    Signed-off-by: jiabin.chen <jiabin.chen@amlogic.com>

--- wifi_bt/wifi/configs/wifi.mk
+++ wifi_bt/wifi/configs/wifi.mk
@@ -263,12 +263,12 @@ endif
 
 ifneq ($(filter w2,$(WIFI_MODULES)),)
 ifeq (,$(wildcard vendor/wifi_driver/amlogic/w2/common))
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.txt,vendor/amlogic/common/wifi_bt/wifi/w2,$(TARGET_COPY_OUT_VENDOR)/etc/wifi/w2/)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.txt,vendor/amlogic/common/wifi_bt/wifi/w2,$(TARGET_COPY_OUT_VENDOR)/lib/firmware/)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.bin,vendor/amlogic/common/wifi_bt/wifi/w2,$(TARGET_COPY_OUT_VENDOR)/lib/firmware/)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.ini,vendor/amlogic/common/wifi_bt/wifi/w2,$(TARGET_COPY_OUT_VENDOR)/lib/firmware/)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.asm,vendor/amlogic/common/wifi_bt/wifi/w2,$(TARGET_COPY_OUT_VENDOR)/lib/firmware/)
 else
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.txt,vendor/wifi_driver/amlogic/w2/common,$(TARGET_COPY_OUT_VENDOR)/etc/wifi/w2/)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.txt,vendor/wifi_driver/amlogic/w2/common,$(TARGET_COPY_OUT_VENDOR)/lib/firmware/)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.bin,vendor/wifi_driver/amlogic/w2/common,$(TARGET_COPY_OUT_VENDOR)/lib/firmware/)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.ini,vendor/wifi_driver/amlogic/w2/common,$(TARGET_COPY_OUT_VENDOR)/lib/firmware/)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.asm,vendor/wifi_driver/amlogic/w2/common,$(TARGET_COPY_OUT_VENDOR)/lib/firmware/)
