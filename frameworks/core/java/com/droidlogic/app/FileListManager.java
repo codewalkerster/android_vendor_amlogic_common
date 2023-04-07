419a3f3d4e0740bb2f567a7af735b4453d60de55
qiujuan.liang <qiujuan.liang@amlogic.com>
2023-04-07 16:15:05 +0800

    file browser:  add 3gpp for video file [1/1]
    PD#SWPL-103079
    
    Problem:
    3gpp not see in filebrowser
    
    Solution:
    add 3gpp to video type
    
    Verify:
    t7
    
    Change-Id: Id32f896654fa418ee4fde9beb1f0735e9a738665
    Signed-off-by: qiujuan.liang <qiujuan.liang@amlogic.com>

--- frameworks/core/java/com/droidlogic/app/FileListManager.java
+++ frameworks/core/java/com/droidlogic/app/FileListManager.java
@@ -102,12 +102,12 @@ public class FileListManager {
 
     /* file type extensions */
     //video
-    private static final String video_extensions = "3gp,asf,avi,dat,divx,f4v,flv,h264,lst,m2ts,m4v,mkv,mp2,mp4,mov,mpe,mpeg,mpg,mts,rm,rmvb,ts,tp,mvc,vc1,vob,wm,wmv,webm,m2v,pmp,bit,h265,3g2,mlv,hm10,ogm,vp9,trp,bin,ivf";
+    private static final String video_extensions = "3gpp,3gp,asf,avi,dat,divx,f4v,flv,h264,lst,m2ts,m4v,mkv,mp2,mp4,mov,mpe,mpeg,mpg,mts,rm,rmvb,ts,tp,mvc,vc1,vob,wm,wmv,webm,m2v,pmp,bit,h265,3g2,mlv,hm10,ogm,vp9,trp,bin,ivf";
 
     //music
     private static final String[] music_extensions = {
         ".mp3", ".wma", ".m4a", ".aac", ".ape", ".mp2", ".ogg", ".flac", ".alac", ".wav",
-        ".mid", ".xmf", ".mka", ".aiff", ".aifc", ".aif", ".pcm", ".adpcm"
+        ".mid", ".xmf", ".mka", ".aiff", ".aifc", ".aif", ".pcm", ".adpcm", ".dsf", ".wsd",".dff"
     };
 
     //photo
