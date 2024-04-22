78f51dc6a924413ec8f89c9752497ac6cfebdefb
wei wang <wei.wang@amlogic.com>
2024-04-22 13:24:18 +0800

    mdns: do not support mdns if api lever is low [1/2]
    PD#SWPL-166441
    
    Problem:
    compile fail
    
    Solution:
    do not include it
    
    Verify:
    Verify it on franklin
    
    Change-Id: Id0e74c08a3aed1c8ac12ef958ea4b5387f632da3
    Signed-off-by: wei wang <wei.wang@amlogic.com>

--- apps/DroidMdnsOffloadService/mdns.mk
+++ apps/DroidMdnsOffloadService/mdns.mk
@@ -2,6 +2,9 @@ SOONG_CONFIG_NAMESPACES += mdns
 SOONG_CONFIG_mdns += \
     board
 
+ifeq ($(TARGET_BUILD_MDNS),false)
+SOONG_CONFIG_mdns_board := mdns_disable
+else
 ifneq ($(wildcard device/google/atv/MdnsOffloadManagerService),)
 PRODUCT_PACKAGES += \
     MdnsOffloadCmdService \
@@ -14,4 +17,5 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/amlogic/common/frameworks/s
 SOONG_CONFIG_mdns_board := mdns_enable
 else
 SOONG_CONFIG_mdns_board := mdns_disable
+endif
 endif
