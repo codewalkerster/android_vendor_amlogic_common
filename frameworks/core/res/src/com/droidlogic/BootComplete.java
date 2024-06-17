44841360c2554a64ac5bedb575e4c33ab0d46a0d
an.xi <an.xi@amlogic.com>
2024-06-17 19:43:18 +0800

    cec: configure soundbar feature with unified switch [1/3]
    PD#SWPL-156020
    
    Problem:
    The soundbar tvconfig is not controlled by unified switch yet.
    
    Solution:
    Enable soundbar feature with "ro.vendor.platform.support.soundbar".
    
    Verify:
    ohm
    
    Change-Id: Ibbfc1cb7b231e519e953e4cfa97fe590205fae2c
    Signed-off-by: an.xi <an.xi@amlogic.com>

--- frameworks/core/res/src/com/droidlogic/BootComplete.java
+++ frameworks/core/res/src/com/droidlogic/BootComplete.java
@@ -50,6 +50,9 @@ public class BootComplete extends BroadcastReceiver {
     private static final String DROID_SETTINGS_PACKAGE = "com.droidlogic.tv.settings";
     private static final String DROID_SETTINGS_ENCRYPTKEEPERFBE = "com.droidlogic.tv.settings.CryptKeeperFBE";
 
+    private static final String SOUNDBAR_MODE = "soundbar_mode";
+    private static final String PROPERTY_SOUNDBAR_MODE_SUPPORTED = "ro.vendor.platform.support.soundbar";
+
     private boolean mHasTvUiMode;
 
     private static final int MSG_SHOW_USB_POWER_STATUS = 0;
@@ -71,6 +74,12 @@ public class BootComplete extends BroadcastReceiver {
         if (SettingsPref.getFirstRun(context)) {
             Log.i(TAG, "first running: " + context.getPackageName());
             SettingsPref.setFirstRun(context, false);
+        }
+
+        boolean soundbarSupported = getBooleanProperty(PROPERTY_SOUNDBAR_MODE_SUPPORTED, false);
+        Log.i(TAG, "soundbar supported:" + soundbarSupported);
+        if (Settings.Global.getInt(resolver, SOUNDBAR_MODE, -1) == -1) {
+            Settings.Global.putInt(resolver, SOUNDBAR_MODE, soundbarSupported ? 1 : 0);
         }
 
         //use to check whether disable camera or not
