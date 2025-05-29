af6c92a587e47ba2221f440b638845e3534c4edc
hong.zhang <hong.zhang@amlogic.com>
2025-05-29 05:06:10 +0000

    WiFi: add iwpriv tool [1/1]
    PD#SWPL-213848
    
    Problem:
    add iwpriv tool
    
    Solution:
    add iwpriv tool
    
    Verify:
    OK
    
    Change-Id: I9a4d821a3b671c1e8d8ab92e93ececb62b291b13
    Signed-off-by: hong.zhang <hong.zhang@amlogic.com>

--- wifi_bt/wifi/configs/wifi.mk
+++ wifi_bt/wifi/configs/wifi.mk
@@ -135,6 +135,7 @@ PRODUCT_COPY_FILES += vendor/amlogic/common/wifi_bt/wifi/multi_wifi/config/p2p_s
 PRODUCT_COPY_FILES += vendor/amlogic/common/wifi_bt/wifi/aml_tools/aml_comm_diag:$(TARGET_COPY_OUT_VENDOR)/xbin/aml_comm_diag
 PRODUCT_COPY_FILES += vendor/amlogic/common/wifi_bt/wifi/aml_tools/parse_trace_msg.sh:$(TARGET_COPY_OUT_VENDOR)/xbin/parse_trace_msg.sh
 PRODUCT_COPY_FILES += vendor/amlogic/common/wifi_bt/wifi/aml_tools/lspci:$(TARGET_COPY_OUT_VENDOR)/xbin/aml_lspci
+PRODUCT_COPY_FILES += vendor/amlogic/common/wifi_bt/wifi/aml_tools/iwpriv:$(TARGET_COPY_OUT_VENDOR)/xbin/iwpriv
 PRODUCT_COPY_FILES += vendor/amlogic/common/wifi_bt/wifi/aml_tools/iwpriv:$(TARGET_COPY_OUT_VENDOR)/xbin/aml_iwpriv
 
 ifneq ($(filter ap6181,$(WIFI_MODULES)),)
